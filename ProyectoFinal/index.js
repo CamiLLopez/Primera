ad0296d87647387dbfd54a932206ea111dd2a8a3
Camila Lopez <camila_lopez_39@hotmail.com>
2021-07-31 11:58:30 +0200

    Mostrando resultados en DOM y almacenando viajes en LocalStorage

--- ProyectoFinal/index.js
+++ ProyectoFinal/index.js
@@ -16,7 +16,7 @@ class Viaje{
         return costePasajero
     }
     resumenViaje(){
-        return "Para realizar el viaje a: "+ this.destino +": el gasto total es de " + this.gastos() + " euros y para realizar el viaje cada pasajero deberá abonarle: " + Math.round(this.costePasajero()) + " euros.";
+        return "Para realizar el viaje a: "+ this.destino +", el gasto total es de " + this.gastos() + " euros y para realizar el viaje cada pasajero deberá abonarle: " + Math.round(this.costePasajero()) + " euros.";
     }
 
 }
@@ -27,41 +27,50 @@ function main(){
     var pasajeros = parseInt(prompt("Cuantos pasajeros van a viajar?"));
     var gasolina = parseInt(prompt("Cuanto pagaran por la gasolina?"));
     var peajes = parseInt(prompt("Cuanto pagarán por el peaje?"));
-    let arrayViajes = []
-    
+    let arrayViajes = [];
+    let continuarViaje;
     //ciclo while para instanciar tantos objetos como viajes quieran hacerse
     while (pasajeros!="" && lugar!=""){
         var viaje = new Viaje(lugar,pasajeros,gasolina,peajes);
         arrayViajes.push(viaje);
-        lugar = prompt("A que destino vas a viajar?");
-        if(lugar===""){
+        localStorage.setItem("viajes", JSON.stringify(arrayViajes));
+        continuarViaje = prompt("Quiere añadir otro viaje?");
+        let respuesta = continuarViaje.toLowerCase();
+        if(respuesta==="no"){
             alert("Gracias por planificar sus viajes con nosostros!");
+            return arrayViajes;
         }else{
+            lugar = prompt("A que destino vas a viajar?");
             pasajeros = parseInt(prompt("Cuantos pasajeros van a viajar?"));
             gasolina = parseInt(prompt("Cuanto pagaran por la gasolina?"));
             peajes = parseInt(prompt("Cuanto pagarán por el peaje?"));
         } 
-    };
-    return arrayViajes;
+    }
 }
 
 //funcion para mostrar resumen de todos los viajes realizados. 
 function mostrarViaje(arrayViajes){
     let opcion = prompt("Quiere ver el listado de viajes guardados?");
+    let opcion1 = opcion.toLowerCase();
 
-    switch(opcion){
+    switch(opcion1){
         case "si":
             for (var i=0; i<arrayViajes.length; i++){
                 var mostrar = arrayViajes[i].resumenViaje();
-                console.log(mostrar);
+                let mensajeValido = document.createElement("p");
+                mensajeValido.innerHTML = `<h3 class="text-success text-justify">${mostrar}</h3>`;
+                document.getElementById("resultados").appendChild(mensajeValido);
             }
             break;
         case "no":
-            console.log("Gracias por utilizar nuestra aplicación, lo esperamos pronto!");
+            let mensaje = document.createElement("p");
+            mensaje.innerHTML = '<h3 class="text-info text-justify">Gracias por utilizar nuestra aplicación, lo esperamos pronto!</h3>';
+            document.getElementById("resultados").appendChild(mensaje);
             break;
         default:
-            console.log("Opcion no valida, responda con si o no. Saliendo de la aplicación");
-            
+            let mensajeInvalido = document.createElement("p");
+            mensajeInvalido.innerHTML ='<h3 class="text-warning text-justify">Opcion no valida, responda con si o no. Saliendo de la aplicación</h3>';
+            document.getElementById("resultados").appendChild(mensajeInvalido);
     };
 }
 
