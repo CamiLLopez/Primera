3f6b8e0dd19c0aa4f0595fdadb035883191756f2
Camila Lopez <camila_lopez_39@hotmail.com>
2021-08-10 18:31:20 +0200

    agregando Jquery al proyecto

--- ProyectoFinal/js/index.js
+++ ProyectoFinal/js/index.js
@@ -1,60 +1,101 @@
-class Viaje{
-    constructor (destino,pasajeros, gasolina, peajes, extra=0){
-        this.destino = destino;
-        this.pasajeros = pasajeros;
-        this.gasolina = gasolina;
-        this.peajes = peajes;
-        this.extra = extra;
+$( document ).ready(function() {
+
+    class Viaje{
+        constructor (destino,pasajeros, gasolina, peajes, extra=0){
+            this.destino = destino;
+            this.pasajeros = pasajeros;
+            this.gasolina = gasolina;
+            this.peajes = peajes;
+            this.extra = extra;
+        }
+        
+        gastos() {
+            var suma = this.gasolina + this.peajes + this.extra;
+            return suma; 
+        } 
+        
+        costePasajero(){
+            var costePasajero =  (this.gastos()) /this.pasajeros;
+            return costePasajero
+        }
+        resumenViaje(){
+            return "Para realizar el viaje a: "+ this.destino +", el gasto total es de " + this.gastos() + " euros y para realizar el viaje cada pasajero deberá abonarle: " + Math.round(this.costePasajero()) + " euros.";
+        }
+    
+    }
+
+    let arrayViajes = [];
+        
+     
+    if (localStorage.getItem('viajes')){
+        arrayViajes = JSON.parse(localStorage.getItem('viajes'));
     }
     
-    gastos() {
-        var suma = this.gasolina + this.peajes + this.extra;
-        return suma; 
-    } 
-    
-    costePasajero(){
-        var costePasajero =  (this.gastos()) /this.pasajeros;
-        return costePasajero
-    }
-    resumenViaje(){
-        return "Para realizar el viaje a: "+ this.destino +", el gasto total es de " + this.gastos() + " euros y para realizar el viaje cada pasajero deberá abonarle: " + Math.round(this.costePasajero()) + " euros.";
+    function guardarViaje(viaje){
+        localStorage.setItem("viajes", JSON.stringify(viaje));
     }
 
-}
+    $('#formularioViajes').submit(function(event){
+        event.preventDefault();
+        let lugar = $('#lugar').val();  
+        let pasajeros = parseInt($('#pasajeros').val());
+        let gasolina = parseInt($('#gasolina').val());
+        let peajes = parseInt($('#peajes').val());
+        let extra = parseInt($('#extra').val());
+        let viaje = new Viaje(lugar,pasajeros,gasolina,peajes, extra);
+        let resumen = viaje.resumenViaje();
+        let viajeUnitario = [viaje, resumen];
+        arrayViajes.push(viajeUnitario);
+        guardarViaje(arrayViajes);
+    })
 
-let arrayViajes = [];
-function main(event){ 
-    event.preventDefault();
-    let lugar = document.getElementById("lugar").value;  
-    let pasajeros =parseInt(document.getElementById("pasajeros").value);
-    let gasolina = parseInt(document.getElementById("gasolina").value);
-    let peajes = parseInt(document.getElementById("peajes").value);
-    let extra = parseInt(document.getElementById("extra").value);
-    let viaje = new Viaje(lugar,pasajeros,gasolina,peajes, extra);
-    let resumen = viaje.resumenViaje();
-    let viajeUnitario = [viaje, resumen];
-    arrayViajes.push(viajeUnitario);
-    localStorage.setItem("viajes", JSON.stringify(arrayViajes));
+  
+    function obtenerViajesStorage(key) {
+        if(localStorage.getItem(key)){
+          return JSON.parse(localStorage.getItem(key));
+        }
+      }
     
-}
-
-function mostrarViaje(){
+    if(!localStorage.getItem('viajes')){
+        crearLista('body', 'listaViajes');
+    }else{
+        crearLista('body', 'listaViajes');
+        crearItemsLista(obtenerViajesStorage('viajes'), '#listaItem');
+    }
     
-    var mostrar = JSON.parse(localStorage.getItem("viajes"));
-    for (let index = 0; index < mostrar.length; index++) {
-        let element = mostrar;
-        let mensajeValido = document.createElement("p");
-        mensajeValido.innerHTML = `<h3 class="text-success text-justify">${element[index][1]}</h3>`;
-        document.getElementById("resultados").appendChild(mensajeValido);
-    }       
- }
+    function crearLista(element, id){
+        const lista = `<li id=${id}></li>`;
+        $(element).append(lista);
+    }
+
+    function crearItemsLista(viajes, id){
+        for (let index = 0; index < viajes.length; index++) {
+            let element = viajes;
+            let mensajeValido = `<ul id=${id}>${element[index][1]}</ui>`;
+            $('#resultados').append(mensajeValido);
+        }
+    }
+
+    function borrarLista(id){
+        $(id).empty();
+    }
+
+    $('#borrarViajes').click(function(){
+        window.localStorage.clear();
+        borrarLista('#resultados');
+    })
+    $('#consultarViajes').click(function(){
+        if(localStorage.getItem('viajes')){
+            crearLista('body', 'listaViajes');
+            crearItemsLista(obtenerViajesStorage('viajes'), '#listaItem');
+        }      
+    })
 
 
-let boton = document.getElementById("enviar");
-boton.addEventListener("click", main);
+});
 
-let verViajes = document.getElementById("consultarViajes");
-verViajes.addEventListener("click", mostrarViaje);
+
+
 
     
   
