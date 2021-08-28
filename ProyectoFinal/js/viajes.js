57054585e8a07ec55bde7c54a1e80c2bbc60cfc1
Camila Lopez <camila_lopez_39@hotmail.com>
2021-08-28 19:38:50 +0200

    llamadas a api hoteles

--- ProyectoFinal/js/viajes.js
+++ ProyectoFinal/js/viajes.js
@@ -10,19 +10,64 @@ $(document).ready(function(){
         const lista = `<ul class="list-group list-group-flush" id=${id}></ul>`;
         $(element).append(lista);
     }
+    function consultarHoteles(indexViaje){
+        let viajesGuardados = obtenerViajesStorage('viajes');
+        if (viajesGuardados != " "){
+            for (let index=0; index<viajesGuardados.length; index++){
+                if (indexViaje === index){
+                    let destinos = viajesGuardados[index][0];
+                    const settings = {
+                        "async": true,
+                        "crossDomain": true,
+                        "url": `https://hotels4.p.rapidapi.com/locations/search?query=${destinos.destino}&locale=en_US`,
+                        "method": "GET",
+                        "headers": {
+                            "x-rapidapi-host": "hotels4.p.rapidapi.com",
+                            "x-rapidapi-key": "3e515ffa67mshd759b56c4fea8d2p1215a3jsnb55607932fff"
+                        }
+                    };
+                    $.ajax(settings).done(function (response) {
+                        let misDatos = response;
+                        for (const dato of misDatos.suggestions) {
+                            if(dato.group =="HOTEL_GROUP"){
+                                if (dato.entities.length<1){
+                                    $("#hotelesResultado").prepend(`<div><h6 class="list-group-item list-group-item-action list-group-item-light">Lo sentimos no existen hoteles para este destino</h3>
+                                        </div>`)
+                                }else{
+                                    for (const entidad of dato.entities){
+                                        $("#hotelesResultado").prepend(`<div><h6 class="list-group-item list-group-item-action list-group-item-light">${entidad.name}</h3>
+                                        <p class="list-group-item list-group-item-action list-group-item-light"> ${entidad.caption}</p>
+                                        </div>`)
+                                }
+                                }
+                                    
+                           }
+                        }
+                }
+                );
+            }
+            }
+        }
+    }
 
     function crearItemsLista(viajes){
         for (let index = 0; index < viajes.length; index++) {
             let element = viajes;
             
-            let mensajeValido =  `<li class="list-group-item list-group-item-action list-group-item-dark" id=${index}>${element[index][1]}
-                                <button class='btn btn-dark btn-m' id="btn${index}" >Delete</button></li>`;
-            
+            let mensajeValido = `<li class="list-group-item list-group-item-action list-group-item-dark" id=${index}>${element[index][1]}
+                                <button class='btn btn-dark btn-m m-3' id="btn${index}" >Borrar viaje</button>
+                                <button class="btn btn-dark btn-m m-3" id="verHoteles${index}">Ver Hoteles!</button>
+                                </li>`;
             $('#resultados').append(mensajeValido);
              $(`#btn${index}`).click(function(e){
                 e.preventDefault();
                 removerViaje(index);
                 $('#consultarViajes').click();
+                })
+                $(`#verHoteles${index}`).click(function(){
+                    $("#hotelesResultado").empty();
+                    consultarHoteles(index);
+                    
                 })
         }
     }
@@ -37,7 +82,7 @@ $(document).ready(function(){
    
     $('#consultarViajes').click(function(){
         if(JSON.parse(localStorage.getItem('viajes')).length<1){
-            const mensaje = `<p>No hay viajes guardados por aqui!</p>`;
+            const mensaje = `<p list-group-item list-group-item-action list-group-item-dark>No hay viajes guardados por aqui!</p>`;
             $('#resultados').append(mensaje);
         }else{
             $('#resultados').empty();
@@ -48,41 +93,5 @@ $(document).ready(function(){
     })
 
 
-    $('#verHoteles').click(function(){
-        let viajesGuardados = obtenerViajesStorage('viajes');
-        if (viajesGuardados != " "){
-            for (let index=0; index<viajesGuardados.length; index++){
-                let destinos = viajesGuardados[index][0];
-                const settings = {
-                    "async": true,
-                    "crossDomain": true,
-                    "url": `https://hotels4.p.rapidapi.com/locations/search?query=${destinos.destino}&locale=en_US`,
-                    "method": "GET",
-                    "headers": {
-                        "x-rapidapi-host": "hotels4.p.rapidapi.com",
-                        "x-rapidapi-key": "3e515ffa67mshd759b56c4fea8d2p1215a3jsnb55607932fff"
-                    }
-                };
-        
-                $.ajax(settings).done(function (response) {
-                    let misDatos = response;
-                    for (const dato of misDatos.suggestions) {
-                        if(dato.group =="HOTEL_GROUP"){
-                            for (const entidad of dato.entities){
-                                $("#resultados").prepend(`<div><h3>${entidad.name}</h3>
-                                <p> ${entidad.caption}</p>
-                                </div>`)
-                            }
-                       }
-                    }
-            }
-            );
-            
-            }
-        }
-
-        
-        
-    })
-
+    
 });
